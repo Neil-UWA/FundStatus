bb81fbe90db6e0137ba803a61acd89fae9942f14
Neil <neil-lu@graduate.uwa.edu.au>
2014-10-21 16:25:07 +0800

    assign url

--- fund-index.js
+++ fund-index.js
@@ -1,21 +1,22 @@
-var cheerio = require('cheerio'),
-    request = require('request'),
-    async = require('async'),
-    util = require('util');
-
-var _fundURL = 'http://fund.eastmoney.com/%s.html',
+var cheerio   = require('cheerio'),
+    request   = require('request'),
+    async     = require('async'),
+    util      = require('util'),
+    fundURL   = 'http://fund.eastmoney.com/%s.html',
     fundLists = process.argv.slice(2);
 
 var getFundStatus = function(fundCode){
-  var fundYesterday;
-  request.get(util.format(_fundURL, fundCode), function(err, res, body){
+  var fundYesterday,
+      url = util.format(fundURL, fundCode);
+
+  request.get(url, function(err, res, body){
     var $ = cheerio.load(body);
     fundYesterday = parseFloat($('.left12').children().first().text());
     console.log("%s 昨日净值：%s", fundCode, fundYesterday);
   });
 
   return function(){
-    request.get(util.format(_fundURL, fundCode), function(err, res, body){
+    request.get(url, function(err, res, body){
       var $ = cheerio.load(body);
       var realTimeFundIndex = parseFloat($('#statuspzgz').children().first().text());
       console.log(fundCode + " | " + realTimeFundIndex + " | "+ _downOrUp(realTimeFundIndex));
