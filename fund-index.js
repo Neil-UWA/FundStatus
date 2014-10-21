818f51300cc04b2f9f92d729a3bd812a2d909eb8
Neil <neil-lu@graduate.uwa.edu.au>
2014-10-21 14:40:03 +0800

    add a start script

--- fund-index.js
+++ fund-index.js
@@ -4,8 +4,9 @@ var request = require('request');
 var fundToday;
 
 var getFundStatus = function(){
-  var _targetURL = 'http://fund.eastmoney.com/161024.html';
-  request.get(_targetURL, function(err, res, body){
+  var _fundURL = 'http://fund.eastmoney.com/161024.html';
+
+  request.get(_fundURL, function(err, res, body){
     var $ = cheerio.load(body);
     var realTimeFundIndex = parseFloat($('#statuspzgz').children().first().text());
     console.log(realTimeFundIndex + " | "+ _downOrUp(realTimeFundIndex));
@@ -18,14 +19,15 @@ var getFundStatus = function(){
 
 var getNetFund = function(){
   var _fullGoalURL = 'http://www.fullgoal.com.cn/funds/zhishu/161024/index.html';
+
   request.get(_fullGoalURL, function(err, res, body){
     var $ = cheerio.load(body);
     fundToday = parseFloat($('.baseinfo').first().children().eq(1).text().split('：')[1]);
-    // console.log($('.baseinfo').first().children().eq(1).text().split('：')[1]);
-     console.log("今日净值："+ fundToday);
+    console.log("今日净值："+ fundToday);
   });
 };
 
 setImmediate(getNetFund);
 setImmediate(getFundStatus);
 setInterval(getFundStatus, 1000*60);
+
