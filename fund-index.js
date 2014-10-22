d085d8cc2dc08e51eeae2cc46ee4850b1d860d96
Neil <neil-lu@graduate.uwa.edu.au>
2014-10-22 10:22:32 +0800

    refactor using prototype

--- fund-index.js
+++ fund-index.js
@@ -9,34 +9,51 @@ var Fund = function(fundCode){
   this.fundCode = fundCode;
   this.fundURL = util.format(baseURL, fundCode);
   this.fundYesterday = 0.0;
+  this.realTimeFundIndex = 0.0;
 };
 
 Fund.prototype.getYesterdayFund = function(){
   var _this = this;
-  request.get(this.fundURL, function(err, res, body){
-    var $ = cheerio.load(body);
-    _this.fundYesterday = parseFloat($('.left12').children().first().text());
-    console.log("%s 昨日净值：%s", _this.fundCode, _this.fundYesterday);
-  });
+
+  try{
+    request.get(this.fundURL, function(err, res, body){
+      if (err) throw new Error(e);
+      var $ = cheerio.load(body);
+      _this.fundYesterday = parseFloat($('.left12').children().first().text());
+      console.log("%s 昨日净值：%s", _this.fundCode, _this.fundYesterday);
+    });
+  } catch(e) {
+    console.error(e);
+  }
 };
 
 Fund.prototype.getFundStatus = function(){
   var _this = this;
-  request.get(this.fundURL, function(err, res, body){
-    var $ = cheerio.load(body);
-    var realTimeFundIndex = parseFloat($('#statuspzgz').children().first().text());
-    console.log(_this.fundCode + " | " + realTimeFundIndex + " | "+ _downOrUp(realTimeFundIndex));
-  });
 
-  _downOrUp = function(realTimeFundIndex){
-    return ((realTimeFundIndex - this.fundYesterday)/this.fundYesterday*100).toPrecision(4) + "%";
+  try{
+    request.get(this.fundURL, function(err, res, body){
+      if (err) throw new Error(err);
+      var $ = cheerio.load(body);
+      _this.realTimeFundIndex = parseFloat($('#statuspzgz').children().first().text());
+      console.log(_this.fundCode + " | " + _this.realTimeFundIndex + " | "+ _downOrUp.call(_this));
+    });
+  } catch(e){
+    console.error(e);
+  }
+
+  function _downOrUp(){
+    return ((this.realTimeFundIndex - this.fundYesterday)/this.fundYesterday*100).toPrecision(4) + "%";
   };
 };
 
 function run(fundCode, callback){
   var fund = new Fund(fundCode);
+
   fund.getYesterdayFund();
-  setInterval(fund.getFundStatus, 1000);
+
+  setInterval(function(){
+    fund.getFundStatus()
+  }, 1000);
 };
 
 async.each(fundLists, run, console.error);
