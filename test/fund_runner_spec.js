cbc2bd235ef1cb485cd9d710a1f5db874062558e
Neil <neil-lu@graduate.uwa.edu.au>
2014-10-22 16:29:05 +0800

    change directory name from tests to test, mocha's best practice

---
+++ test/fund_runner_spec.js
@@ -0,0 +1,17 @@
+var chai    = require('chai'),
+    expect  = chai.expect,
+    util    = require('util'),
+    sinon   = require('sinon'),
+    FundRunner = require('../lib/fund_runner');
+
+describe('FundRunner', function(){
+  describe('constructor', function(){
+    context('when no fund code is passed', function(){
+      it('returns default fund code');
+    });
+
+    context('when fund codes are passed', function(){
+      it('returns default fund code');
+    });
+  });
+});
